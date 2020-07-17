47e436aaa02c59d19078518c092a4637a2f5c991
Mosi <50152599+hasanian01@users.noreply.github.com>
2020-07-17 19:07:17 -0400

    Add files via upload

---
+++ src/aaaaa.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+file=open('output.txt','a')
+file.write('Mostafa is here'+'   \n')
+file.close()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[65]:
+
+
+
+
+
+# In[66]:
+
+
+
+
+
+# In[45]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
