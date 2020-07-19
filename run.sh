68cf64e607af53135a1d4ef8d335875996e1b22c
Mosi <50152599+hasanian01@users.noreply.github.com>
2020-07-19 01:13:28 -0400

    Delete run.sh

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#
-# Use this shell script to compile (if necessary) your code and then execute it. Belw is an example of what might be found in this file if your program was written in Python 3.7
-# python3.7 ./src/aaaaaa.py ./input/complaints.csv ./output/report.csv
