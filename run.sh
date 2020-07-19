5f33739c9a73be3677e1893c3c7f86c6f89f0944
Mosi <50152599+hasanian01@users.noreply.github.com>
2020-07-19 01:14:10 -0400

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# I ran it on python 3.8
+clear
+python3 ./src/consumer_complaints.py ./input/complaints.csv ./output/report.csv
+echo '--------------------------------------------------'
+echo 'Mostafa Hasanian'
+
