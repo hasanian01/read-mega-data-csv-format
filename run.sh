9c10f1e6fb31e26e85a1900fcbbb31c3075d30e9
Mosi <50152599+hasanian01@users.noreply.github.com>
2020-07-20 00:11:06 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 #
 # I ran it on python 3.8
 clear
-python3 ./src/consumer_complaints.py ./input/complaints.csv ./output/report.csv
+python3.7 ./src/consumer_complaints.py ./input/complaints.csv ./output/report.csv
 echo '--------------------------------------------------'
 echo 'Mostafa Hasanian'
 
